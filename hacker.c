b6b455f8b1940200d65723ee95c98bca96023098
bambooclc <815152985@qq.com>
2024-09-21 00:56:09 +0800

    Add files via upload

--- hacker.c
+++ hacker.c
@@ -3,14 +3,12 @@
 int main() {
     short code;
     for (;;) {
-        printf("Show me your code, please.\n"); // 添加换行符\n
-        scanf("%hd", &code); // 使用%hd以匹配short类型
-        if (code >= 100000 && code <= 999999) {
+        printf("Show me your code, please.\n"); 
+        scanf("%hd", &code); 
+        if (code >= 100000 && code <= 999999) 
             printf("I am super hacker!\n");
-        } else {
+        else 
             printf("Fake code!\n");
-        }
-        if (code >= 100000 || (code < 100000 && code > 999999)) break; // 分开判断防止误判
     }
-    return 012;
+    return 0;
 }
